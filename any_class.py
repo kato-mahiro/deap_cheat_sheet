5b3e8774a1324a6b70151f23db922731f2709bf7
katomasahiro10 <katomasahiro10@gmail.com>
2019-08-07 10:02:06 +0700

    still disable

--- any_class.py
+++ any_class.py
@@ -7,19 +7,26 @@ from deap import creator
 from deap import tools
 
 class myclass:
-    def __init__(self,hoge):
+    def __init__(self):
         self.gene1 = numpy.random.rand(2,2)
         self.gene2 = "gene"
         self.gene3 = random.choice([0,1])
 
+def create_ind_uniform():
+    ind = []
+    ind.append(myclass())
+    return ind
+
 creator.create("FitnessMax", base.Fitness, weights=(1.0,))
-creator.create("Individual", myclass, fitness=creator.FitnessMax)
+creator.create("Individual", numpy.ndarray, fitness=creator.FitnessMax)
 
 toolbox = base.Toolbox()
 
 #toolbox.register("attr_bool", random.randint, 0, 1)
-toolbox.register("attr_myclass", myclass,None)
-toolbox.register("individual", tools.initRepeat, creator.Individual, toolbox.attr_myclass, n=1)
+#toolbox.register("individual", tools.initRepeat, creator.Individual, toolbox.attr_myclass, n=1)
+
+toolbox.register("create_ind", create_ind_uniform, )
+toolbox.register("individual", tools.initIterate, creator.Individual, toolbox.create_ind)
 toolbox.register("population", tools.initRepeat, list, toolbox.individual)
 
 def evalOneMax(individual):
